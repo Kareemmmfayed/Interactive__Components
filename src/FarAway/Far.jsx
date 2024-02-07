54c8a3c496410f5c1f20a4437bae68dd7f651f18
kareem <124769561+Kareemmmfayed@users.noreply.github.com>
2024-02-07 04:04:44 +0200

    Done!

--- src/FarAway/Far.jsx
+++ src/FarAway/Far.jsx
@@ -1,62 +1,171 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
 import "./index.css";
-
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "Charger", quantity: 1, packed: true },
-];
 
 function Logo() {
   return <h1>🌴 Far Away 🎒</h1>;
 }
 
-function Form() {
+function Form({ onAddItem }) {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = {
+      id: Date.now(),
+      description: description,
+      quantity: quantity,
+      packed: false,
+    };
+
+    onAddItem(newItem);
+
+    setDescription("");
+    setQuantity(1);
+  };
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-    </div>
+      <select
+        value={quantity}
+        onChange={(e) => {
+          setQuantity(Number(e.target.value));
+        }}
+      >
+        {Array.from({ length: 20 }, (_, i) => (
+          <option value={i + 1} key={i + 1}>
+            {i + 1}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="item..."
+        value={description}
+        onChange={(e) => {
+          setDescription(e.target.value);
+        }}
+      />
+      <button>Add</button>
+    </form>
   );
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteItem, onToggleItem }) {
   return (
     <li>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => onToggleItem(item.id)}
+      />
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button>❌</button>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
 
-function PackingList() {
+function PackingList({ items, onDeleteItem, onToggleItem, onClearList }) {
+  const [sortBy, setSortBy] = useState("input");
+  let sortedItems;
+  if (sortBy == "input") sortedItems = items;
+  if (sortBy == "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  if (sortBy == "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => {
-          return <Item key={item.id} item={item} />;
-        })}
+        {sortedItems.map((item) => (
+          <Item
+            key={item.id}
+            item={item}
+            onDeleteItem={onDeleteItem}
+            onToggleItem={onToggleItem}
+          />
+        ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+        <button onClick={onClearList}>Clear list</button>
+      </div>
     </div>
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list! 🚀</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>🛍️ You have X items on your list, and you already packed X</em>
+      <em>
+        {percentage == 100
+          ? "You packed everything! ready to go ✈️"
+          : `🛍️ You have ${numItems} items on your list, and you already packed ${numPacked}, (${percentage} %)`}
+      </em>
     </footer>
   );
 }
 
 function Far() {
+  const [items, setItems] = useState([]);
+
+  const handleAddItem = (item) => {
+    setItems((prevItems) => [...prevItems, item]);
+  };
+
+  const handleDeleteItem = (id) => {
+    setItems((prevItems) => prevItems.filter((item) => item.id !== id));
+  };
+
+  const handleToggleItem = (id) => {
+    setItems((prevItems) =>
+      prevItems.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  };
+
+  const handleClearList = () => {
+    const confirmed = window.confirm("Are you sure you wanna clear the list?");
+    if (confirmed) setItems([]);
+  };
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
-      <Stats />
+      <Form onAddItem={handleAddItem} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
+      />
+      <Stats items={items} />
     </div>
   );
 }
